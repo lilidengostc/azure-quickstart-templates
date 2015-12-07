ec2011150165e7df9afd31152b84b24132325975
Bin Xia <binxi@microsoft.com>
2015-12-07 09:48:00 +0000

    Will not enable DNS on devbox
    1. Users can use xip.io if they don't have their own domain name
    2. Users can use Azure DNS service if they have their own domain name

--- bosh-setup/setup_devbox.py
+++ bosh-setup/setup_devbox.py
@@ -65,24 +65,3 @@ call("chmod 400 {0}/bosh".format(home_dir), shell=True)
 # Install bosh_cli and bosh-init
 #call("rm -r /tmp; mkdir /mnt/tmp; ln -s /mnt/tmp /tmp; chmod 777 /mnt/tmp; chmod 777 /tmp", shell=True)
 call("mkdir /mnt/bosh_install; cp init.sh /mnt/bosh_install; cd /mnt/bosh_install; chmod +x init.sh; ./init.sh >{0} 2>&1;".format(install_log), shell=True)
-
-# Setup the devbox as a DNS
-enable_dns = settings["enable-dns"]
-if enable_dns:
-    try:
-        import urllib2
-        cf_ip = settings["cf-ip"]
-        dns_ip = re.search('\d+\.\d+\.\d+\.\d+', urllib2.urlopen("http://www.whereismyip.com").read()).group(0)
-        call("python setup_dns.py -d cf.azurelovecf.com -i 10.0.16.4 -e {0} -n {1} >/dev/null 2>&1".format(cf_ip, dns_ip), shell=True)
-        # Update motd
-        call("cp -f 98-msft-love-cf /etc/update-motd.d/", shell=True)
-        call("chmod 755 /etc/update-motd.d/98-msft-love-cf", shell=True)
-    except Exception as e:
-        err_msg = "\nWarning:\n"
-        err_msg += "\nFailed to setup DNS with error: {0}, {1}".format(e, traceback.format_exc())
-        err_msg += "\nYou can setup DNS manually with \"python setup_dns.py -d cf.azurelovecf.com -i 10.0.16.4 -e External_IP_of_CloudFoundry -n External_IP_of_Devbox\""
-        err_msg += "\nExternal_IP_of_CloudFoundry can be found in {0}/settings.".format(home_dir)
-        err_msg += "\nExternal_IP_of_Devbox is the dynamic IP which can be found in Azure Portal."
-        with open(install_log, 'a') as f:
-            f.write(err_msg)
-
