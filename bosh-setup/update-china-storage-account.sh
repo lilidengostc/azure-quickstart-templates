e96d896bb90a2119c67c4a6b9d4f67fc8f532310
Bin Xia <binxi@microsoft.com>
2016-08-19 03:33:55 +0000

    Inject a xip.io record into postgres

--- bosh-setup/update-china-storage-account.sh
+++ bosh-setup/update-china-storage-account.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+if [[ $# -ne 1 || -z "$1" ]]; then
+    echo "Usage: ./update-china-storage-account.sh <template-version>. For example: ./update-china-storage-account.sh v1-0-0"
+    exit 1
+fi
+
+set -e
+
 # The template version should be same as variables('templateVersion') in azuredeploy.json
 template_version="$1"
 
 azure storage blob upload scripts/create_cert.sh bosh-setup ${template_version}/scripts/create_cert.sh --quiet
 azure storage blob upload scripts/deploy_bosh.sh bosh-setup ${template_version}/scripts/deploy_bosh.sh --quiet
 azure storage blob upload scripts/deploy_cloudfoundry.sh bosh-setup ${template_version}/scripts/deploy_cloudfoundry.sh --quiet
+azure storage blob upload scripts/inject_xip_io_records.py bosh-setup ${template_version}/scripts/inject_xip_io_records.py --quiet
 azure storage blob upload scripts/init.sh bosh-setup ${template_version}/scripts/init.sh --quiet
 azure storage blob upload scripts/setup_env bosh-setup ${template_version}/scripts/setup_env --quiet
 azure storage blob upload scripts/setup_env.py bosh-setup ${template_version}/scripts/setup_env.py --quiet
